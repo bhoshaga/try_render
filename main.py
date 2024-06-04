82c5c93da0d37156b386bb9aaa7097be810d6847
bhoshaga <72672555+bhoshaga@users.noreply.github.com>
2024-06-03 20:07:53 -0700

    v1.1

--- main.py
+++ main.py
@@ -219,6 +219,6 @@ async def websocket_endpoint(websocket: WebSocket):
     except WebSocketDisconnect:
         print("Client disconnected")
 
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == '__main__':
+#     import uvicorn
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
